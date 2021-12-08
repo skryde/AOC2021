e9fe3892ae021d8d6dd65649a62a889fc393f6c3
Sebastián <sebart.07@gmail.com>
2021-12-08 13:48:57 -0300

    Se cambia el comportamiento de la función que lee los archivos de entrada para aliminar la última línea vacía que tienen.

--- utils/read.go
+++ utils/read.go
@@ -11,5 +11,8 @@ func ReadFile(fileName string) ([]string, error) {
 		return nil, err
 	}
 
-	return strings.Split(string(data), "\n"), nil
+	// Se eliminan los blank characters que puedan haber al inicio o al final del string.
+	// Particularmente me interesa eliminar la ultima linea en blanco que tiene el archivo de entrada.
+	noBlankChars := strings.TrimSpace(string(data))
+	return strings.Split(noBlankChars, "\n"), nil
 }
